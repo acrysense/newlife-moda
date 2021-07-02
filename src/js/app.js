939fc26c3d15c54c39809309272850f7c3d1745f
acrysense <54480354+acrysense@users.noreply.github.com>
2021-07-02 12:02:08 +0300

    fix: Improvements #21

--- src/js/app.js
+++ src/js/app.js
@@ -388,4 +388,64 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+
+    // MODAL
+    const modalBtn = document.querySelectorAll('.modal-btn'),
+        modal = document.querySelectorAll('.modal'),
+        modalClose = document.querySelectorAll('.modal__close'),
+        modalOverlay = document.querySelector('.overlay')
+    
+    if (modalBtn) {
+        modalBtn.forEach((item, i) => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                document.querySelectorAll('.modal.modal--active').forEach((child) => child.classList.remove('modal--active'))
+                if (!modalOverlay.classList.contains('modal-overlay--active')) {
+                    modalOverlay.classList.add('modal-overlay--active')
+                }
+                document.body.classList.add('scroll-disabled')
+                document.querySelector('.header').classList.add('header--active')
+
+                const modalID = item.dataset.id
+                document.getElementById(modalID).classList.add('modal--active')
+            });
+        });
+    }
+
+    document.body.addEventListener('keyup', (event) => {
+        let key = event.keyCode;
+
+        if (modal && modalOverlay.classList.contains('modal-overlay--active')) {
+            if (key == 27) {
+                document.body.classList.remove('scroll-disabled')
+                document.querySelector('.header').classList.remove('header--active')
+                document.querySelectorAll('.modal.modal--active').forEach((child) => child.classList.remove('modal--active'))
+                modalOverlay.classList.remove('modal-overlay--active')
+            };
+        }
+    }, false);
+
+
+    if (modalOverlay) {
+        modalOverlay.addEventListener('click', () => {
+            if (modal && modalOverlay.classList.contains('modal-overlay--active')) {
+                document.body.classList.remove('scroll-disabled')
+                document.querySelector('.header').classList.remove('header--active')
+                document.querySelectorAll('.modal.modal--active').forEach((child) => child.classList.remove('modal--active'))
+                modalOverlay.classList.remove('modal-overlay--active')
+            }
+        });
+    }
+
+    if (modalClose) {
+        modalClose.forEach((item) => {
+            item.addEventListener('click', () => {
+                document.body.classList.remove('scroll-disabled')
+                document.querySelector('.header').classList.remove('header--active')
+                document.querySelectorAll('.modal.modal--active').forEach((child) => child.classList.remove('modal--active'))
+                modalOverlay.classList.remove('modal-overlay--active')
+            });
+        });
+    }
 });
