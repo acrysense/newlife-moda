490c39cec44afaa99e59902cdd83bfb555e9c345
acrysense <54480354+acrysense@users.noreply.github.com>
2021-08-11 18:05:56 +0300

    fix: Update product card

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,79 @@ document.addEventListener('DOMContentLoaded', function () {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
+
+    // SMOOTH SCROOL
+    function currentYPosition() {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+        
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+
+        return 0;
+    } 
+    
+    function elmYPosition(eID) {
+        let elm = document.getElementById(eID);
+        let y = elm.offsetTop;
+        let node = elm;
+        while (node.offsetParent && node.offsetParent != document.body) {
+            node = node.offsetParent;
+            y += node.offsetTop;
+        } return y;
+    }
+    
+    function smoothScroll(eID) {
+        let startY = currentYPosition();
+        let stopY = elmYPosition(eID) - Number(document.querySelector('.header').clientHeight);
+        let distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        let speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        let step = Math.round(distance / 25);
+        let leapY = stopY > startY ? startY + step : startY - step;
+        let timer = 0;
+        if (stopY > startY) {
+            for (let i = startY; i < stopY; i += step ) {
+                setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for (let i = startY; i > stopY; i -= step ) {
+            setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+    }
+
+    function smoothScrollCoord(coord) {
+        let startY = currentYPosition();
+        let stopY = coord;
+        let distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        let speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        let step = Math.round(distance / 25);
+        let leapY = stopY > startY ? startY + step : startY - step;
+        let timer = 0;
+        if (stopY > startY) {
+            for (let i = startY; i < stopY; i += step ) {
+                setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for (let i = startY; i > stopY; i -= step ) {
+            setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+    }
     
     // MOBILE MENU
     const hamburger = document.getElementById('hamburger-toggle')
@@ -220,6 +293,25 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    const galleryThumbImg = document.querySelectorAll('.product__gallery-thumb > .product__gallery-img');
+    const galleryMainImg = document.querySelectorAll('.product__gallery-main > .product__gallery-img');
+    const sections = {};
+
+    galleryMainImg.forEach((item, i) => {
+        sections[i] = item.offsetTop + product.offsetTop - 100;
+    })
+
+    galleryThumbImg.forEach((item, i) => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault()
+
+            document.querySelectorAll('.product__gallery-thumb > .product__gallery-img').forEach((child) => child.classList.remove('is--active'))
+            item.classList.add('is--active');
+
+            smoothScrollCoord(sections[i])
+        })
+    })
+
     window.addEventListener('scroll', () => {
         if (topPromo && searchWrapper) {
             const topPromoHeight = topPromo.getBoundingClientRect().height;
@@ -266,6 +358,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         productScroll()
+
+        for (let i in sections) {
+            if (sections[i] <= window.pageYOffset) {
+                document.querySelectorAll('.product__gallery-thumb > .product__gallery-img').forEach((child) => child.classList.remove('is--active'))
+                document.querySelectorAll('.product__gallery-thumb > .product__gallery-img')[i].classList.add('is--active');
+            }
+        }
     })
     
     window.addEventListener('resize', () => {
@@ -287,9 +386,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             }
         }
-
-        //transformVal = productRightHeight - document.documentElement.clientHeight + 30
-        //productRight.style.transform = 'translate3d(0px, -' + transformVal + 'px, 0px)'
     })
 
     if (searchBtn) {
